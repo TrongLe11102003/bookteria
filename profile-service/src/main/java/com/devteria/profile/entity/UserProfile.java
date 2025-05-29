dfc69609e8e582795082ae4c67a0e7b62d375871
Trong Le <tronglevan1110@gmail.com>
2025-05-29 17:36:41 +0700

    add-openfeign-client

--- profile-service/src/main/java/com/devteria/profile/entity/UserProfile.java
+++ profile-service/src/main/java/com/devteria/profile/entity/UserProfile.java
@@ -1,15 +1,14 @@
 package com.devteria.profile.entity;
 
-import java.time.LocalDate;
-
+import lombok.*;
+import lombok.experimental.FieldDefaults;
 import org.springframework.data.neo4j.core.schema.GeneratedValue;
 import org.springframework.data.neo4j.core.schema.Id;
 import org.springframework.data.neo4j.core.schema.Node;
 import org.springframework.data.neo4j.core.schema.Property;
 import org.springframework.data.neo4j.core.support.UUIDStringGenerator;
 
-import lombok.*;
-import lombok.experimental.FieldDefaults;
+import java.time.LocalDate;
 
 @Getter
 @Setter
