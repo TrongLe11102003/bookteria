4bf46a365a8bb51e7a24610dcddaf52e312b50a7
Trong Le <tronglevan1110@gmail.com>
2025-06-10 19:58:28 +0700

    create-post-and-pagination

--- notification-service/src/main/java/com/devteria/notification/service/EmailService.java
+++ notification-service/src/main/java/com/devteria/notification/service/EmailService.java
@@ -30,8 +30,8 @@ public class EmailService {
     public EmailResponse sendEmail(SendEmailRequest request) {
         EmailRequest emailRequest = EmailRequest.builder()
                 .sender(Sender.builder()
-                        .name("Devteria DotCom")
-                        .email("devteriadotcom@gmail.com")
+                        .name("Trong DotCom")
+                        .email("tronglevan1110@gmail.com")
                         .build())
                 .to(List.of(request.getTo()))
                 .subject(request.getSubject())
--- post-service/src/main/java/com/devteria/post/service/PostService.java
+++ post-service/src/main/java/com/devteria/post/service/PostService.java
@@ -1,5 +1,6 @@
 package com.devteria.post.service;
 
+import com.devteria.post.dto.PageResponse;
 import com.devteria.post.dto.request.PostRequest;
 import com.devteria.post.dto.response.PostResponse;
 import com.devteria.post.entity.Post;
@@ -8,12 +9,14 @@ import com.devteria.post.repository.PostRepository;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -36,13 +39,19 @@ public class PostService {
         return postMapper.toPostResponse(post);
     }
 
-    public List<PostResponse> getMyPosts(){
+    public PageResponse<PostResponse> getMyPosts(int page, int size){
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         String userId = authentication.getName();
+        Sort sort = Sort.by("createDate").descending();
+        Pageable pageable = PageRequest.of(page - 1, size, sort);
+        var pageData = postRepository.findAllByUserId(userId, pageable);
 
-        return postRepository.findAllByUserId(userId)
-                .stream()
-                .map(postMapper::toPostResponse)
-                .toList();
+        return PageResponse.<PostResponse>builder()
+                .currentPage(page)
+                .pageSize(pageData.getSize())
+                .totalPages(pageData.getTotalPages())
+                .totalElements(pageData.getTotalElements())
+                .data(pageData.getContent().stream().map(postMapper::toPostResponse).toList())
+                .build();
     }
 }
