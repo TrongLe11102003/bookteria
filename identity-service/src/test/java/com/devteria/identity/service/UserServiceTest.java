3c490f16c6c3bfe0d2a8ae50b6293a12015dfde9
Trong Le <tronglevan1110@gmail.com>
2025-07-01 20:51:25 +0700

    add-websocket

--- identity-service/src/test/java/com/devteria/identity/service/UserServiceTest.java
+++ identity-service/src/test/java/com/devteria/identity/service/UserServiceTest.java
@@ -49,21 +49,21 @@ public class UserServiceTest {
                 .dob(dob)
                 .build();
 
-        userResponse = UserResponse.builder()
-                .id("cf0600f538b3")
-                .username("john")
-                .firstName("John")
-                .lastName("Doe")
-                .dob(dob)
-                .build();
-
-        user = User.builder()
-                .id("cf0600f538b3")
-                .username("john")
-                .firstName("John")
-                .lastName("Doe")
-                .dob(dob)
-                .build();
+        //        userResponse = UserResponse.builder()
+        //                .id("cf0600f538b3")
+        //                .username("john")
+        //                .firstName("John")
+        //                .lastName("Doe")
+        //                .dob(dob)
+        //                .build();
+        //
+        //        user = User.builder()
+        //                .id("cf0600f538b3")
+        //                .username("john")
+        //                .firstName("John")
+        //                .lastName("Doe")
+        //                .dob(dob)
+        //                .build();
     }
 
     @Test
